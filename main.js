dbfc571249e4fb39990ace906411a5cd95c75835
Ferney Morales <fmoralesvi@unadvirtual.edu.co>
2025-07-29 08:45:39 -0500

    Algunos cambios

--- main.js
+++ main.js
@@ -10,3 +10,15 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+
+// main.js
+
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
